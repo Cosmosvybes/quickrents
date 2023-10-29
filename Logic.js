2320f3cf6fade5352d19cc6d5639fe0ab788ca06
Cosmos <Alfredchrisayo@gmail.com>
2023-10-29 14:38:33 +0000

    commented module

--- Logic.js
+++ Logic.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
 const { config } = require("dotenv");
 config();
+
 const client = new MongoClient(process.env.MONGO_URI, {
   monitorCommands: true,
 });
