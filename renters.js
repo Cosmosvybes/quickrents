c331dfdc4cd6f8c65be623a0112e84bfe00408d8
Cosmos <Alfredchrisayo@gmail.com>
2023-10-29 14:30:41 +0000

    checking

--- renters.js
+++ renters.js
@@ -6,42 +6,42 @@ const client = new MongoClient(process.env.MONGO_URI, {
 });
 const customers = client.db("quickrents").collection("customers");
 
- async function newApplication(firstname, lastname, state) {
-  const response = await customers.insertOne({
-    id: Math.floor(Math.random() * 765),
-    firstname: firstname,
-    lastname: lastname,
-    state: state,
-    status: "Pending",
-    year: new Date().getFullYear(),
-  });
-  return response;
-}
+//  async function newApplication(firstname, lastname, state) {
+//   const response = await customers.insertOne({
+//     id: Math.floor(Math.random() * 765),
+//     firstname: firstname,
+//     lastname: lastname,
+//     state: state,
+//     status: "Pending",
+//     year: new Date().getFullYear(),
+//   });
+//   return response;
+// }
 
- async function getCustomers() {
-  const data = await customers.find({}).toArray();
-  return data;
-}
+//  async function getCustomers() {
+//   const data = await customers.find({}).toArray();
+//   return data;
+// }
 
-async function processApplication(id) {
-  const response = await customers.updateOne(
-    { id: Number(id) },
-    { $set: { status: "Processing" } }
-  );
-  return response;
-}
+// async function processApplication(id) {
+//   const response = await customers.updateOne(
+//     { id: Number(id) },
+//     { $set: { status: "Processing" } }
+//   );
+//   return response;
+// }
 
- async function approveApplication(id) {
-  const response = await customers.updateOne(
-    { id: id },
-    { $set: { status: "Approved" } }
-  );
-  return response;
-}
+//  async function approveApplication(id) {
+//   const response = await customers.updateOne(
+//     { id: id },
+//     { $set: { status: "Approved" } }
+//   );
+//   return response;
+// }
 
-module.exports = {
-  getCustomers,
-  newApplication,
-  processApplication,
-  approveApplication,
-};
+// module.exports = {
+//   getCustomers,
+//   newApplication,
+//   processApplication,
+//   approveApplication,
+// };
