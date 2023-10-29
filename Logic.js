a7efab4f2bf079ca46d99b4e8bccf610b0a280d1
Cosmos <Alfredchrisayo@gmail.com>
2023-10-29 14:35:46 +0000

    create new file

---
+++ Logic.js
@@ -0,0 +1,47 @@
+const { MongoClient } = require("mongodb");
+const { config } = require("dotenv");
+config();
+const client = new MongoClient(process.env.MONGO_URI, {
+  monitorCommands: true,
+});
+const customers = client.db("quickrents").collection("customers");
+
+ async function newApplication(firstname, lastname, state) {
+  const response = await customers.insertOne({
+    id: Math.floor(Math.random() * 765),
+    firstname: firstname,
+    lastname: lastname,
+    state: state,
+    status: "Pending",
+    year: new Date().getFullYear(),
+  });
+  return response;
+}
+
+ async function getCustomers() {
+  const data = await customers.find({}).toArray();
+  return data;
+}
+
+async function processApplication(id) {
+  const response = await customers.updateOne(
+    { id: Number(id) },
+    { $set: { status: "Processing" } }
+  );
+  return response;
+}
+
+ async function approveApplication(id) {
+  const response = await customers.updateOne(
+    { id: id },
+    { $set: { status: "Approved" } }
+  );
+  return response;
+}
+
+module.exports = {
+  getCustomers,
+  newApplication,
+  processApplication,
+  approveApplication,
+};
