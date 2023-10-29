3a41a26b843bd8d8aca2c8cedbac0e5691bbadc1
Cosmos <Alfredchrisayo@gmail.com>
2023-10-29 15:00:12 +0000

    changed

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const { config } = require("dotenv");
+
 config();
 const port = process.env.PORT || 2000;
 const { sendMail, sendApproval, sendProcessing } = require("./Confirmation");
