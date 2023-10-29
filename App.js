7723f3bf80fa22b77c1e7b7395326601f4e937c3
Cosmos <Alfredchrisayo@gmail.com>
2023-10-29 14:58:08 +0000

    testing

--- App.js
+++ App.js
@@ -7,6 +7,52 @@ const port = process.env.PORT || 2000;
 const { sendMail, sendApproval, sendProcessing } = require("./Confirmation");
 // const { approveApplication } = require("./Logic");
 
+const { MongoClient } = require("mongodb");
+const { config } = require("dotenv");
+config();
+
+const client = new MongoClient(
+  "mongodb+srv://cosmos:ayomide22689@cosmoscluster.o6ovlp8.mongodb.net/",
+  {
+    monitorCommands: true,
+  }
+);
+
+const customers = client.db("quickrents").collection("customers");
+
+async function newApplication(firstname, lastname, state) {
+  const response = await customers.insertOne({
+    id: Math.floor(Math.random() * 765),
+    firstname: firstname,
+    lastname: lastname,
+    state: state,
+    status: "Pending",
+    year: new Date().getFullYear(),
+  });
+  return response;
+}
+
+async function getCustomers() {
+  const data = await customers.find({}).toArray();
+  return data;
+}
+
+async function processApplication(id) {
+  const response = await customers.updateOne(
+    { id: Number(id) },
+    { $set: { status: "Processing" } }
+  );
+  return response;
+}
+
+async function approveApplication(id) {
+  const response = await customers.updateOne(
+    { id: id },
+    { $set: { status: "Approved" } }
+  );
+  return response;
+}
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:1616");
   res.setHeader(
